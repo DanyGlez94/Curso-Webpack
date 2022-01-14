4495c4daa8eeda2aa9aff606b05a156944ef8034
DanyGlez94 <daniel_glez94@hotmail.com>
2022-01-14 12:27:29 -0600

    Agrgeada la configuración de babel y el archivo webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+//Este archivo va a ser nuestro recurso principal
+
+const path = require('path'); //Para trabajar con archivos y rutas de directorios.
+//import {resolve} from 'path';
+
+/** @type {import('webpack').Configuration} */ //Añade autocompletado a nuestro archivo de webpack
+
+module.exports = { //creamos un módulo que se va a exportar
+    entry: './src/index.js', //El punto de entrada de nuestra app
+    output: { 
+        path: path.resolve(__dirname, 'dist'), //Utilizamos el path que declaramos arriba, resolve() nos permite saber dónde se encuentra nuestro proyecto.
+        filename: 'main.js', //Nombre del archivo final
+    },
+    resolve: {
+        extensions: ['.js'] //los archivos que webpack podrá leer
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/, //Expresión regular para utilizar solamente archivos .mjs y .js.
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
